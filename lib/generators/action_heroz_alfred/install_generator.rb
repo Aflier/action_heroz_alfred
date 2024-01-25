e517e21687f03527788a444862ff76df9d2f7de1
Simon Kiteley <simon@aflier.com>
2024-01-25 13:58:04 +0000

    Initial Checkin

--- lib/generators/action_heroz_alfred/install_generator.rb
+++ lib/generators/action_heroz_alfred/install_generator.rb
@@ -4,7 +4,7 @@ module ActionHerozAlfred
       source_root File.expand_path("../templates", __FILE__)
 
       def create_initializer_file
-        template "javascript/controllers/help_slider_controller.js", "app/javascript/controllers/help_slider_controller.js"
+        template "app/javascript/controllers/help_slider_controller.js", "app/javascript/controllers/help_slider_controller.js"
         template "app/views/_action_heroz_button.html.erb", "app/views/action_heroz_alfred/_action_heroz_button.html.erb"
         template "app/views/_action_heroz_slide.html.erb", "app/views/action_heroz_alfred/_action_heroz_slide.html.erb"
       end
