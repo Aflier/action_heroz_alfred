cbbc9c4417dbb485192d2d9d4c0ca931b4e110aa
Simon Kiteley <simon@aflier.com>
2024-01-25 13:57:13 +0000

    Initial Checkin

--- lib/generators/action_heroz_alfred/install_generator.rb
+++ lib/generators/action_heroz_alfred/install_generator.rb
@@ -4,18 +4,6 @@ module ActionHerozAlfred
       source_root File.expand_path("../templates", __FILE__)
 
       def create_initializer_file
-        # template "position_control.rb", "app/controllers/concerns/position_control.rb"
-
-        # template "assets/stylesheets/layout.scss", "app/assets/stylesheets/layout.scss"
-
-        # template "models/concerns/super_table_filter.rb", "app/models/concerns/super_table_filter.rb"
-        # template "javascript/controllers/filter_controller.js", "app/javascript/controllers/filter_controller.js"
-        # template "javascript/controllers/filters_controller.js", "app/javascript/controllers/filters_controller.js"
-        # template "views/stimulus/filters/_filter_input.html.erb", "app/views/stimulus/filters/_filter_input.html.erb"
-        # template "views/stimulus/filters/_filter_number_input.html.erb", "app/views/stimulus/filters/_filter_number_input.html.erb"
-        # template "views/stimulus/filters/_filter_radio.html.erb", "app/views/stimulus/filters/_filter_radio.html.erb"
-        # template "views/stimulus/filters/_filter_toggle.html.erb", "app/views/stimulus/filters/_filter_toggle.html.erb"
-
         template "javascript/controllers/help_slider_controller.js", "app/javascript/controllers/help_slider_controller.js"
         template "app/views/_action_heroz_button.html.erb", "app/views/action_heroz_alfred/_action_heroz_button.html.erb"
         template "app/views/_action_heroz_slide.html.erb", "app/views/action_heroz_alfred/_action_heroz_slide.html.erb"
