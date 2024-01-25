3e46020b71291ed3bbaac95124b74adf0aa34c9c
Simon Kiteley <simon@aflier.com>
2024-01-25 12:56:19 +0000

    Initial Checkin

---
+++ lib/action_heroz_alfred.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "action_heroz_alfred/version"
+
+module ActionHerozAlfred
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "action_heroz_alfred"
+
+require "minitest/autorun"
